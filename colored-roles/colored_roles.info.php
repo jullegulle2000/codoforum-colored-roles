bb025f67126402a1950371928acfd85a2517f578
jullegulle2000 <33057837+jullegulle2000@users.noreply.github.com>
2017-10-25 15:26:48 +0200

    Create colored_roles.info.php

---
+++ colored-roles/colored_roles.info.php
@@ -0,0 +1,10 @@
+<?php
+
+  $info['name'] = 'Colored Roles';
+  $info['plugin_type']= 'block'; //tell the forum that it is a block  
+  $info['description'] = 'Changes the colors of users depending on their role';
+  $info['version'] = '1.x';
+  $info['author'] = "Julle";
+  $info['author_url'] = 'http://dbdsnetwork.tk';
+  $info['license'] = 'MIT License';
+  $info['core'] = '1.x'; //works with which version of the forum?
